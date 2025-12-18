978366c47f85fa053e779169735f0512c0ff4e92
Biswaranjan Giri <goodlegiri892@gmail.com>
2025-12-18 22:50:29 +0530

    Fix frontend API endpoints to match new backend marketing routes

--- studioflow/client/src/lib/marketing.js
+++ studioflow/client/src/lib/marketing.js
@@ -3,24 +3,24 @@ import api from './api';
 export const marketingApi = {
   // Leads
   subscribe: async (data) => {
-    return api.post('/leads/subscribe', data);
+    return api.post('/marketing/leads/subscribe', data);
   },
   
   verifyLead: async (token) => {
-    return api.get(`/leads/verify/${token}`);
+    return api.get(`/marketing/leads/verify/${token}`);
   },
 
   // Feedback
   submitFeedback: async (data, tokenGetter) => {
-    return api.post('/feedback', data, { getToken: tokenGetter });
+    return api.post('/marketing/feedback', data, { getToken: tokenGetter });
   },
 
   // Content
   getPosts: async (type) => { // type: 'blog' | 'changelog'
-    return api.get(`/content/${type}`);
+    return api.get(`/marketing/content/${type}`);
   },
 
   getPostBySlug: async (type, slug, tokenGetter) => {
-    return api.get(`/content/${type}/${slug}`, { getToken: tokenGetter });
+    return api.get(`/marketing/content/${type}/${slug}`, { getToken: tokenGetter });
   }
 };
